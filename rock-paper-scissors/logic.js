d2122c4c8ba784f20ac39526b362a4ee2596b15c
Vincent <vincentlucht04@gmail.com>
2023-12-29 22:21:13 +0100

    Fiexed variable scope and optimised code in getUserInput() function

--- rock-paper-scissors/logic.js
+++ rock-paper-scissors/logic.js
@@ -1,20 +1,24 @@
 function getUserInput() {
-    let userChoice;
+    userChoice;
     let buttonDiv = document.querySelector("#buttonDiv");
     buttonDiv.addEventListener("click", (event) => {
         let target = event.target;
 
         switch(target.id) {
             case "rock":
-                console.log("Rock");
-                return userChoice = "Rock";
+                let rock = "Rock";
+                break;
+
             case "paper":
-                console.log("Paper");
-                return userChoice = "Paper";
+                userChoice = "Paper";
+                break;
+
             case "scissors":
-                console.log("Scissors")
-                return userChoice = "Scissors";
+                userChoice = "Scissors";
+                break;
         }
+        let comChoice = getComputerChoice();
+        playRound(userChoice=userChoice, comChoice=comChoice);
     })
 }
 
@@ -23,37 +27,33 @@ function getComputerChoice() {
 
     // generate random Choice
     let randomChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    console.log(`The computer chose: ${randomChoice}`);
+    //console.log(`The computer chose: ${randomChoice}`);
     return randomChoice;
 }
 
 function playRound(userChoice, comChoice) {
-    // convert first letter to uppercase
-    let userChoiceUpper = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
-    let comChoiceUpper = comChoice.charAt(0).toUpperCase() + comChoice.slice(1);
-
     // replay round if draw
     if (userChoice == comChoice) {
-        console.log(`${userChoiceUpper} vs ${comChoiceUpper}, it's a draw!`);
+        console.log(`${userChoice} vs ${comChoice}, it's a draw!`);
     } else {
             // rock beats scissors
-        if ((userChoice == "rock" && comChoice == "scissors") ||
+        if ((userChoice == "Rock" && comChoice == "Scissors") ||
             // paper beats rock
-            (userChoice == "paper" && comChoice == "rock") ||
+            (userChoice == "Paper" && comChoice == "Rock") ||
             // scissors beats paper
-            (userChoice == "scissors" && comChoice == "paper")) {
-            console.log(`${userChoiceUpper} beats ${comChoiceUpper}, you get 1 point`);
+            (userChoice == "Scissors" && comChoice == "Paper")) {
+            console.log(`${userChoice} beats ${comChoice}, you get 1 point`);
             point += 1;
         } else {
-            console.log(`${comChoiceUpper} beats ${userChoiceUpper}, the computer gets 1 point`);
+            console.log(`${comChoice} beats ${userChoice}, the computer gets 1 point`);
             comPoint += 1;
         }
     }
+    console.log(`User: ${point} || Computer: ${comChoice}`);
 }
 
 // initialize points
 point = 0;
 comPoint = 0;
-
-// play the game
+let userChoice;
 getUserInput()
