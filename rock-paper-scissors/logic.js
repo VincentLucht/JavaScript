2bf0ad82e4f7b8f0c4bcec0d1ebd5f9669ab0caa
Vincent <vincentlucht04@gmail.com>
2023-12-30 11:40:02 +0100

    Fix playRound() bug Fixed bug, where it created a new result each time, which cluttered the UI

--- rock-paper-scissors/logic.js
+++ rock-paper-scissors/logic.js
@@ -24,20 +24,16 @@ function getUserInput() {
 
 function getComputerChoice() {
     let computerOptions = ["Rock", "Paper", "Scissors"];
-
-    // generate random Choice
     let randomChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    //console.log(`The computer chose: ${randomChoice}`);
     return randomChoice;
 }
 
 function playRound(userChoice, comChoice) {
     let div = document.querySelector(".results")
-    let result = document.createElement("p");
+    let result = document.querySelector("#result");
     // replay round if draw
     if (userChoice == comChoice) {
         result.textContent = `${userChoice} vs ${comChoice}, it's a draw!`;
-        div.appendChild(result);
     } else {
             // rock beats scissors
         if ((userChoice == "Rock" && comChoice == "Scissors") ||
@@ -46,15 +42,12 @@ function playRound(userChoice, comChoice) {
             // scissors beats paper
             (userChoice == "Scissors" && comChoice == "Paper")) {
             result.textContent = `${userChoice} beats ${comChoice}, you get 1 point`;
-            div.append(result)
             point += 1;
         } else {
             result.textContent = `${comChoice} beats ${userChoice}, the computer gets 1 point`;
-            div.append(result)
             comPoint += 1;
         }
     }
-    console.log(`User: ${point} || Computer: ${comChoice}`);
     updateScore();
 }
 
@@ -68,6 +61,7 @@ function updateScore() {
     let spanCom = document.querySelector("#computerscore");
     spanCom.textContent = comScore;
 }
+
 // initialize points
 point = 0;
 comPoint = 0;
