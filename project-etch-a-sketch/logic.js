4edcc0ce881960daf3cb7026347245e1349a3b32
Vincent <vincentlucht04@gmail.com>
2023-12-31 12:46:45 +0100

    Add hover effect over grid

--- project-etch-a-sketch/logic.js
+++ project-etch-a-sketch/logic.js
@@ -7,6 +7,15 @@ for (let i = 1; i <= 16; i++){
     for (let j = 1; j <= 16; j++) {
         let div = document.createElement("div");
         div.textContent = i;
+
+        /* add event listener to change color to a div */
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = "red";
+        });
+        div.addEventListener("mouseout", function() {
+            div.style.backgroundColor = "";
+        });
+
         divContainer.appendChild(div)
         mainDiv.appendChild(divContainer);
     }
