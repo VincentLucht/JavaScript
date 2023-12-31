f8224ad8f8ae79515063ac4cda251b136ddc500a
Vincent <vincentlucht04@gmail.com>
2023-12-31 11:52:19 +0100

    Complete 16x16 grid
    - added another loop to correctly create a 16x16
    - add appropiate css to make the divs grow to the screen size

--- project-etch-a-sketch/logic.js
+++ project-etch-a-sketch/logic.js
@@ -2,13 +2,12 @@
 let mainDiv = document.querySelector(".mainDiv");
 
 for (let i = 1; i <= 16; i++){
+    let divContainer = document.createElement("div");
+    divContainer.className = "container"
     for (let j = 1; j <= 16; j++) {
         let div = document.createElement("div");
-        div.textContent = j;
-        div.style.width = "50px";
-        div.style.height = "50px";
-        mainDiv.appendChild(div);
+        div.textContent = i;
+        divContainer.appendChild(div)
+        mainDiv.appendChild(divContainer);
     }
 }
-
-// use css to break the line
