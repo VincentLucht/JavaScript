7ddd2381432a7de8d9400fb5f0fda52b1dcb8ed3
Vincent <vincentlucht04@gmail.com>
2024-01-01 15:34:51 +0100

    Complete notes from javascript.info

--- fundamentals-part-5/objects(dicts)/declaring.js
+++
@@ -1,7 +0,0 @@
-// can either use this syntax
-let obj = new Object();
-
-// or this syntax
-let user = {};
-
-// declaring an object
---
+++ fundamentals-part-5/objects(dicts)/js.infoTasks.js
@@ -0,0 +1,10 @@
+// Create an empty object user.
+let user = {};
+
+// Add the property name with the value John.
+user.name = "John"
+console.log(user.name)
+
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
